ebab2433ad11ab1919b48ced0b9d655e7a5724d1
atikahadzman <atikah.adzman@gmail.com>
2026-08-03 05:36:12 +0800

    improve book of the month section

--- src/pages/dashboard/BookOfMonth.jsx
+++ src/pages/dashboard/BookOfMonth.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function BookOfMonth({ book }) {
     if (!book) {
         return (
-            <div className="main-card bg-[#0b8270] rounded-md py-5 font-poppins">
+            <div className="main-card bg-[#f9f3ee] rounded-md py-5 font-poppins">
                 <div className="card-body">
                     <h3 className="text-2xl font-bold text-white">
                         Loading book of the month...
@@ -14,9 +14,9 @@ export default function BookOfMonth({ book }) {
     }
 
     return (
-        <div className="main-card bg-[#0b8270] rounded-md py-5 font-poppins">
+        <div className="main-card bg-[#fec600] rounded-md py-5 font-poppins">
             <div className="card-body">
-                <h3 className="text-2xl font-bold text-white">
+                <h3 className="text-2xl font-bold text-[#111111]">
                     Book of the Month
                 </h3>
 
@@ -33,22 +33,22 @@ export default function BookOfMonth({ book }) {
                 </div>
 
                 {/* title and status */}
-                <div className="flex-1 w-full justify-center items-center">
-                    <div className="flex flex-col md:flex-row md:items-center gap-2">
-                        <div className="flex gap-2">
-                            <p className="text-xl font-semibold text-white hover:translate-x-1 transition-transform">
-                                {book?.title}
-                            </p>
-                        </div>
-                    </div>
-                    <p className="text-sm text-white">{book?.author}</p>
+                <div className="flex-1 w-full flex flex-col items-center text-center space-y-4 mt-5 text-[#111111]">
+                    <p className="text-xl font-semibold hover:translate-x-1 transition-transform">
+                        {book?.title}
+                    </p>
+
+                    <p className="text-sm">
+                        by {book?.author}
+                    </p>
 
                     <div className="flex justify-center items-center space-y-4 mt-5">
-                        <button
-                            className="border-2 border-white text-white px-6 py-3 hover:bg-white hover:text-blue-600 transition"
+                        <Link 
+                            to={`/book/${book.id}`}
+                            className="border-2 border-indigo-900 text-indigo-900 px-3 py-3 hover:bg-white hover:text-blue-600 transition"
                         >
                             Start reading
-                        </button>
+                        </Link>
                     </div>
                 </div>
             </div>
